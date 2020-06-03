c55a07821d4f3673a12692ac3623cfe71acc948f
Max Kasprzik <m.kasprzik04@gmail.com>
2020-06-03 13:27:13 -0700

    using jquery .load to render repeated html segments

--- sole_purpose/static/main.js
+++ sole_purpose/static/main.js
@@ -1,13 +1,13 @@
 // JQUERY
-$('.carousel').carousel()
-AOS.init();
-
 $(document).ready(function(){
+    $('.carousel').carousel()
     // $("#read-more-link").click(function() {
     //     $("#popdown-info").slideToggle();
     // })
-
-})
+    $("#navbar").load("navbar.html");
+    $("#footer-contact").load("footer_contact.html");
+    // $("#test-asdf").html("<p>testing paragraph</p>");
+});
 
 // JAVASCRIPT
 function test() {
